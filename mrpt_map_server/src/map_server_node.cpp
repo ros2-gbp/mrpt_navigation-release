3940aeeae85bdd91593c4d454425f80eac519cdb
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-09-25 14:20:14 +0200

    Imported upstream version '2.2.0' of 'upstream'

--- mrpt_map_server/src/map_server_node.cpp
+++ mrpt_map_server/src/map_server_node.cpp
@@ -6,8 +6,7 @@
    | All rights reserved. Released under BSD 3-Clause license. See LICENSE  |
    +------------------------------------------------------------------------+ */
 
-#include "mrpt_map_server/map_server_node.h"
-
+#include <geometry_msgs/msg/transform_stamped.h>
 #include <mrpt/config/CConfigFile.h>
 #include <mrpt/core/lock_helper.h>
 #include <mrpt/io/CFileGZInputStream.h>
@@ -20,11 +19,15 @@
 #include <mrpt/maps/CVoxelMap.h>
 #include <mrpt/ros2bridge/map.h>
 #include <mrpt/ros2bridge/point_cloud2.h>
+#include <mrpt/ros2bridge/pose.h>
 #include <mrpt/ros2bridge/time.h>
 #include <mrpt/serialization/CArchive.h>
 #include <mrpt/system/filesystem.h>	 // ASSERT_FILE_EXISTS_()
+#include <mrpt/topography/conversions.h>
+#include <mrpt_map_server/map_server_node.h>
 
 #include <nav_msgs/msg/occupancy_grid.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 using namespace mrpt::config;
 using mrpt::maps::CMultiMetricMap;
@@ -46,15 +49,12 @@ void MapServer::init()
 	std::string map_yaml_file;
 	this->declare_parameter<std::string>("map_yaml_file", "");
 	this->get_parameter("map_yaml_file", map_yaml_file);
-	RCLCPP_INFO(
-		this->get_logger(), "map_yaml_file name: '%s'", map_yaml_file.c_str());
+	RCLCPP_INFO(this->get_logger(), "map_yaml_file name: '%s'", map_yaml_file.c_str());
 
 	std::string mrpt_metricmap_file;
 	this->declare_parameter<std::string>("mrpt_metricmap_file", "");
 	this->get_parameter("mrpt_metricmap_file", mrpt_metricmap_file);
-	RCLCPP_INFO(
-		this->get_logger(), "mrpt_metricmap_file name: '%s'",
-		mrpt_metricmap_file.c_str());
+	RCLCPP_INFO(this->get_logger(), "mrpt_metricmap_file name: '%s'", mrpt_metricmap_file.c_str());
 
 	auto lck = mrpt::lockHelper(theMapMtx_);
 
@@ -94,15 +94,13 @@ void MapServer::init()
 		ASSERT_FILE_EXISTS_(mm_file);
 
 		RCLCPP_INFO_STREAM(
-			this->get_logger(),
-			"Loading metric_map_t map from '" << mm_file << "' ...");
+			this->get_logger(), "Loading metric_map_t map from '" << mm_file << "' ...");
 
 		bool mapReadOk = theMap_.load_from_file(mm_file);
 		ASSERT_(mapReadOk);
 
 		RCLCPP_INFO_STREAM(
-			this->get_logger(),
-			"Loaded map contents: " << theMap_.contents_summary());
+			this->get_logger(), "Loaded map contents: " << theMap_.contents_summary());
 	}
 
 	this->declare_parameter<std::string>("frame_id", frame_id_);
@@ -111,13 +109,11 @@ void MapServer::init()
 
 	this->declare_parameter<std::string>("pub_mm_topic", pub_mm_topic_);
 	this->get_parameter("pub_mm_topic", pub_mm_topic_);
-	RCLCPP_INFO(
-		this->get_logger(), "pub_mm_topic: '%s'", pub_mm_topic_.c_str());
+	RCLCPP_INFO(this->get_logger(), "pub_mm_topic: '%s'", pub_mm_topic_.c_str());
 
 	this->declare_parameter<std::string>("service_map", service_map_str_);
 	this->get_parameter("service_map", service_map_str_);
-	RCLCPP_INFO(
-		this->get_logger(), "service_map: '%s'", service_map_str_.c_str());
+	RCLCPP_INFO(this->get_logger(), "service_map: '%s'", service_map_str_.c_str());
 
 	srvMapLayers_ = this->create_service<mrpt_nav_interfaces::srv::GetLayers>(
 		this->get_fully_qualified_name() + "/get_layers"s,
@@ -126,23 +122,22 @@ void MapServer::init()
 			mrpt_nav_interfaces::srv::GetLayers::Response::SharedPtr res)
 		{ srv_map_layers(req, res); });
 
-	srvGetGrid_ = this->create_service<
-		mrpt_nav_interfaces::srv::GetGridmapLayer>(
+	srvGetGrid_ = this->create_service<mrpt_nav_interfaces::srv::GetGridmapLayer>(
 		this->get_fully_qualified_name() + "/get_grid_layer"s,
 		[this](
-			const mrpt_nav_interfaces::srv::GetGridmapLayer::Request::SharedPtr
-				req,
+			const mrpt_nav_interfaces::srv::GetGridmapLayer::Request::SharedPtr req,
 			mrpt_nav_interfaces::srv::GetGridmapLayer::Response::SharedPtr res)
 		{ srv_get_gridmap(req, res); });
 
-	srvGetPoints_ = this->create_service<
-		mrpt_nav_interfaces::srv::GetPointmapLayer>(
+	srvGetPoints_ = this->create_service<mrpt_nav_interfaces::srv::GetPointmapLayer>(
 		this->get_fully_qualified_name() + "/get_pointcloud_layer"s,
 		[this](
-			const mrpt_nav_interfaces::srv::GetPointmapLayer::Request::SharedPtr
-				req,
+			const mrpt_nav_interfaces::srv::GetPointmapLayer::Request::SharedPtr req,
 			mrpt_nav_interfaces::srv::GetPointmapLayer::Response::SharedPtr res)
 		{ srv_get_pointmap(req, res); });
+
+	// Create TF broadcaster:
+	tf_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(*this);
 }
 
 void MapServer::publish_map()
@@ -178,9 +173,8 @@ void MapServer::publish_map()
 		// 2.1) for any map, publish it in mrpt binary form:
 		if (pubLayers_.count(layerName) == 0)
 		{
-			pubLayers_[layerName].pub =
-				this->create_publisher<mrpt_msgs::msg::GenericObject>(
-					pub_mm_topic_ + "/"s + layerName, QoS);
+			pubLayers_[layerName].pub = this->create_publisher<mrpt_msgs::msg::GenericObject>(
+				pub_mm_topic_ + "/"s + layerName, QoS);
 		}
 
 		{
@@ -191,9 +185,7 @@ void MapServer::publish_map()
 
 		// 2.2) publish as ROS standard msgs, if applicable too:
 		// Is it a pointcloud?
-		if (auto pts =
-				std::dynamic_pointer_cast<mrpt::maps::CPointsMap>(layerMap);
-			pts)
+		if (auto pts = std::dynamic_pointer_cast<mrpt::maps::CPointsMap>(layerMap); pts)
 		{
 			if (pubPointMaps_.count(layerName) == 0)
 			{
@@ -203,17 +195,14 @@ void MapServer::publish_map()
 			}
 
 			{
-				const sensor_msgs::msg::PointCloud2 msg_pts =
-					pointmap_layer_to_msg(pts);
+				const sensor_msgs::msg::PointCloud2 msg_pts = pointmap_layer_to_msg(pts);
 
 				pubPointMaps_[layerName].pub->publish(msg_pts);
 			}
 		}
 
 		// Is it a voxelmap?
-		if (auto vxl =
-				std::dynamic_pointer_cast<mrpt::maps::CVoxelMap>(layerMap);
-			vxl)
+		if (auto vxl = std::dynamic_pointer_cast<mrpt::maps::CVoxelMap>(layerMap); vxl)
 		{
 			// Render in RVIZ as occupied voxels=points:
 			mrpt::maps::CSimplePointsMap::Ptr pts = vxl->getOccupiedVoxels();
@@ -233,21 +222,16 @@ void MapServer::publish_map()
 		}
 
 		// Is it a grid map?
-		if (auto grid =
-				std::dynamic_pointer_cast<mrpt::maps::COccupancyGridMap2D>(
-					layerMap);
-			grid)
+		if (auto grid = std::dynamic_pointer_cast<mrpt::maps::COccupancyGridMap2D>(layerMap); grid)
 		{
 			if (pubGridMaps_.count(layerName) == 0)
 			{
-				pubGridMaps_[layerName].pub =
-					this->create_publisher<nav_msgs::msg::OccupancyGrid>(
-						pub_mm_topic_ + "/"s + layerName + "_gridmap"s, QoS);
+				pubGridMaps_[layerName].pub = this->create_publisher<nav_msgs::msg::OccupancyGrid>(
+					pub_mm_topic_ + "/"s + layerName + "_gridmap"s, QoS);
 
 				pubGridMapsMetaData_[layerName].pub =
 					this->create_publisher<nav_msgs::msg::MapMetaData>(
-						pub_mm_topic_ + "/"s + layerName + "_gridmap_metadata"s,
-						QoS);
+						pub_mm_topic_ + "/"s + layerName + "_gridmap_metadata"s, QoS);
 			}
 
 			{
@@ -259,6 +243,92 @@ void MapServer::publish_map()
 		}
 
 	}  // end for each layer
+
+	// Publish the geo-referenced metadata:
+	// 1/2: as mrpt serialized object
+	{
+		// If it's nullopt, it will be serialized correctly as such.
+		mrpt::io::CMemoryStream memBuf;
+		auto arch = mrpt::serialization::archiveFrom(memBuf);
+
+		arch << theMap_.georeferencing;
+
+		if (!pubGeoRef_.pub)
+		{
+			pubGeoRef_.pub = this->create_publisher<mrpt_msgs::msg::GenericObject>(
+				pub_mm_topic_ + "/geo_ref"s, QoS);
+		}
+
+		mrpt_msgs::msg::GenericObject msg;
+		msg.data.resize(memBuf.getTotalBytesCount());
+		::memcpy(msg.data.data(), memBuf.getRawBufferData(), msg.data.size());
+		pubGeoRef_.pub->publish(msg);
+	}
+
+	// Now, only if there is valid geo-ref data, publish /tf's for
+	// ENU and UTM:
+	// See: https://docs.mola-slam.org/latest/geo-referencing.html
+
+	mrpt_nav_interfaces::msg::GeoreferencingMetadata geoMsg;
+
+	if (theMap_.georeferencing)
+	{
+		// ENU -> MAP
+		const auto T_enu_to_map = theMap_.georeferencing->T_enu_to_map.mean;
+		geometry_msgs::msg::TransformStamped tfMsg;
+
+		tfMsg.header.stamp = this->get_clock()->now();
+		tfMsg.header.frame_id = "enu";	// parent
+		tfMsg.child_frame_id = "map";  // child
+		tfMsg.transform = tf2::toMsg(mrpt::ros2bridge::toROS_tfTransform(T_enu_to_map));
+		tf_broadcaster_->sendTransform(tfMsg);
+
+		// ENU -> UTM
+		mrpt::topography::TUTMCoords utmCoordsOfENU;
+		int utmZone = 0;
+		char utmBand = 0;
+		mrpt::topography::GeodeticToUTM(
+			theMap_.georeferencing->geo_coord, utmCoordsOfENU, utmZone, utmBand);
+
+		// T_enu_to_utm = - utmCoordsOfENU  (without rotation, both are "ENU")
+		const auto T_enu_to_utm = mrpt::poses::CPose3D::FromTranslation(-utmCoordsOfENU);
+
+		tfMsg.header.frame_id = "enu";	// parent
+		tfMsg.child_frame_id = "utm";  // child
+		tfMsg.transform = tf2::toMsg(mrpt::ros2bridge::toROS_tfTransform(T_enu_to_utm));
+		tf_broadcaster_->sendTransform(tfMsg);
+
+		geoMsg.t_enu_to_map =
+			tf2::toMsg(mrpt::ros2bridge::toROS_Pose(theMap_.georeferencing->T_enu_to_map));
+		geoMsg.t_enu_to_utm = tf2::toMsg(mrpt::ros2bridge::toROS_Pose(T_enu_to_utm));
+		geoMsg.utm_zone = utmZone;
+		geoMsg.utm_band = std::string{utmBand};
+	}
+
+	// 2/2: as ROS msg
+	if (!pubGeoRefMsg_.pub)
+	{
+		pubGeoRefMsg_.pub =
+			this->create_publisher<mrpt_nav_interfaces::msg::GeoreferencingMetadata>(
+				pub_mm_topic_ + "/geo_ref_metadata"s, QoS);
+	}
+
+	if (theMap_.georeferencing)
+	{
+		auto g = *theMap_.georeferencing;
+
+		geoMsg.valid = true;
+
+		geoMsg.latitude = g.geo_coord.lat.decimal_value;
+		geoMsg.longitude = g.geo_coord.lon.decimal_value;
+		geoMsg.height = g.geo_coord.height;
+		// the rest of fields are already filled in above.
+	}
+	else
+	{
+		geoMsg.valid = false;
+	}
+	pubGeoRefMsg_.pub->publish(geoMsg);
 }
 
 void MapServer::loop()
@@ -277,15 +347,12 @@ void MapServer::srv_map_layers(
 	(void)req;
 
 	resp->layers.clear();
-	for (const auto& [layerName, _] : theMap_.layers)
-		resp->layers.push_back(layerName);
+	for (const auto& [layerName, _] : theMap_.layers) resp->layers.push_back(layerName);
 }
 
 void MapServer::srv_get_gridmap(
-	const std::shared_ptr<mrpt_nav_interfaces::srv::GetGridmapLayer::Request>
-		req,
-	const std::shared_ptr<mrpt_nav_interfaces::srv::GetGridmapLayer::Response>
-		resp)
+	const std::shared_ptr<mrpt_nav_interfaces::srv::GetGridmapLayer::Request> req,
+	const std::shared_ptr<mrpt_nav_interfaces::srv::GetGridmapLayer::Response> resp)
 {
 	auto lck = mrpt::lockHelper(theMapMtx_);
 
@@ -307,8 +374,7 @@ void MapServer::srv_get_gridmap(
 }
 
 void MapServer::srv_get_pointmap(
-	const std::shared_ptr<mrpt_nav_interfaces::srv::GetPointmapLayer::Request>
-		req,
+	const std::shared_ptr<mrpt_nav_interfaces::srv::GetPointmapLayer::Request> req,
 	std::shared_ptr<mrpt_nav_interfaces::srv::GetPointmapLayer::Response> resp)
 {
 	auto lck = mrpt::lockHelper(theMapMtx_);
@@ -339,29 +405,22 @@ sensor_msgs::msg::PointCloud2 MapServer::pointmap_layer_to_msg(
 
 	sensor_msgs::msg::PointCloud2 msg_pts;
 
-	if (auto* xyzirt =
-			dynamic_cast<const mrpt::maps::CPointsMapXYZIRT*>(pts.get());
-		xyzirt)
+	if (auto* xyzirt = dynamic_cast<const mrpt::maps::CPointsMapXYZIRT*>(pts.get()); xyzirt)
 	{
 		mrpt::ros2bridge::toROS(*xyzirt, msg_header, msg_pts);
 	}
-	else if (auto* xyzi =
-				 dynamic_cast<const mrpt::maps::CPointsMapXYZI*>(pts.get());
-			 xyzi)
+	else if (auto* xyzi = dynamic_cast<const mrpt::maps::CPointsMapXYZI*>(pts.get()); xyzi)
 	{
 		mrpt::ros2bridge::toROS(*xyzi, msg_header, msg_pts);
 	}
-	else if (auto* sPts =
-				 dynamic_cast<const mrpt::maps::CSimplePointsMap*>(pts.get());
-			 sPts)
+	else if (auto* sPts = dynamic_cast<const mrpt::maps::CSimplePointsMap*>(pts.get()); sPts)
 	{
 		mrpt::ros2bridge::toROS(*sPts, msg_header, msg_pts);
 	}
 	else
 	{
 		THROW_EXCEPTION_FMT(
-			"Unexpected point cloud class: '%s'",
-			pts->GetRuntimeClass()->className);
+			"Unexpected point cloud class: '%s'", pts->GetRuntimeClass()->className);
 	}
 
 	return msg_pts;
