48aaa4f5530ff20ff872d0ca322c3f544137ca00
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-09-25 14:20:17 +0200

    Rebase from 'upstream'

--- mrpt_map_server/include/mrpt_map_server/map_server_node.h
+++ mrpt_map_server/include/mrpt_map_server/map_server_node.h
@@ -11,16 +11,18 @@
 #include <mp2p_icp/metricmap.h>
 #include <mrpt/config/CConfigFile.h>
 #include <mrpt/maps/CMultiMetricMap.h>
+#include <tf2_ros/static_transform_broadcaster.h>
 
-#include "mrpt_msgs/msg/generic_object.hpp"
-#include "mrpt_nav_interfaces/srv/get_gridmap_layer.hpp"
-#include "mrpt_nav_interfaces/srv/get_layers.hpp"
-#include "mrpt_nav_interfaces/srv/get_pointmap_layer.hpp"
-#include "nav_msgs/msg/map_meta_data.hpp"
-#include "nav_msgs/msg/occupancy_grid.hpp"
-#include "nav_msgs/srv/get_map.hpp"
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/point_cloud2.hpp"
+#include <mrpt_msgs/msg/generic_object.hpp>
+#include <mrpt_nav_interfaces/msg/georeferencing_metadata.hpp>
+#include <mrpt_nav_interfaces/srv/get_gridmap_layer.hpp>
+#include <mrpt_nav_interfaces/srv/get_layers.hpp>
+#include <mrpt_nav_interfaces/srv/get_pointmap_layer.hpp>
+#include <nav_msgs/msg/map_meta_data.hpp>
+#include <nav_msgs/msg/occupancy_grid.hpp>
+#include <nav_msgs/srv/get_map.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 
 class MapServer : public rclcpp::Node
 {
@@ -38,10 +40,7 @@ class MapServer : public rclcpp::Node
 
 	std::string frame_id_ = "map";
 
-	rclcpp::Service<nav_msgs::srv::GetMap>::SharedPtr
-		m_service_map;	//!< service for map server
-	nav_msgs::srv::GetMap::Response
-		m_response_ros;	 //!< response from the map server
+	std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_broadcaster_;
 
 	/// metric map: will be used whatever is the incoming map format.
 	mp2p_icp::metric_map_t theMap_;
@@ -86,6 +85,9 @@ class MapServer : public rclcpp::Node
 	// for the top-level mm metric map:
 	PerTopicData<mrpt_msgs::msg::GenericObject> pubMM_;
 
+	PerTopicData<mrpt_msgs::msg::GenericObject> pubGeoRef_;
+	PerTopicData<mrpt_nav_interfaces::msg::GeoreferencingMetadata> pubGeoRefMsg_;
+
 	// clang-format off
 	// Binary form of each layer:
 	std::map<std::string, PerTopicData<mrpt_msgs::msg::GenericObject>> pubLayers_;
@@ -98,34 +100,24 @@ class MapServer : public rclcpp::Node
 
 	// clang-format on
 
-	sensor_msgs::msg::PointCloud2 pointmap_layer_to_msg(
-		const mrpt::maps::CPointsMap::Ptr& pts);
+	sensor_msgs::msg::PointCloud2 pointmap_layer_to_msg(const mrpt::maps::CPointsMap::Ptr& pts);
 
 	// Services:
-	rclcpp::Service<mrpt_nav_interfaces::srv::GetLayers>::SharedPtr
-		srvMapLayers_;
+	rclcpp::Service<mrpt_nav_interfaces::srv::GetLayers>::SharedPtr srvMapLayers_;
 
 	void srv_map_layers(
 		const std::shared_ptr<mrpt_nav_interfaces::srv::GetLayers::Request> req,
 		std::shared_ptr<mrpt_nav_interfaces::srv::GetLayers::Response> resp);
 
-	rclcpp::Service<mrpt_nav_interfaces::srv::GetGridmapLayer>::SharedPtr
-		srvGetGrid_;
+	rclcpp::Service<mrpt_nav_interfaces::srv::GetGridmapLayer>::SharedPtr srvGetGrid_;
 
 	void srv_get_gridmap(
-		const std::shared_ptr<
-			mrpt_nav_interfaces::srv::GetGridmapLayer::Request>
-			req,
-		std::shared_ptr<mrpt_nav_interfaces::srv::GetGridmapLayer::Response>
-			resp);
+		const std::shared_ptr<mrpt_nav_interfaces::srv::GetGridmapLayer::Request> req,
+		std::shared_ptr<mrpt_nav_interfaces::srv::GetGridmapLayer::Response> resp);
 
-	rclcpp::Service<mrpt_nav_interfaces::srv::GetPointmapLayer>::SharedPtr
-		srvGetPoints_;
+	rclcpp::Service<mrpt_nav_interfaces::srv::GetPointmapLayer>::SharedPtr srvGetPoints_;
 
 	void srv_get_pointmap(
-		const std::shared_ptr<
-			mrpt_nav_interfaces::srv::GetPointmapLayer::Request>
-			req,
-		std::shared_ptr<mrpt_nav_interfaces::srv::GetPointmapLayer::Response>
-			resp);
+		const std::shared_ptr<mrpt_nav_interfaces::srv::GetPointmapLayer::Request> req,
+		std::shared_ptr<mrpt_nav_interfaces::srv::GetPointmapLayer::Response> resp);
 };
